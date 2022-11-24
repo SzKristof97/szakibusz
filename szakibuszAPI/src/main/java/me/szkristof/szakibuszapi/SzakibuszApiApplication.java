7ffe86a5395d3c7bf23d0a070d83306fce4de763
Szabó Kristóf Gyula <szkristofgy@gmail.com>
2022-11-24 14:56:19 +0100

    project init + pom.xml update

---
+++ szakibuszAPI/src/main/java/me/szkristof/szakibuszapi/SzakibuszApiApplication.java
@@ -0,0 +1,13 @@
+package me.szkristof.szakibuszapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzakibuszApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzakibuszApiApplication.class, args);
+    }
+
+}
