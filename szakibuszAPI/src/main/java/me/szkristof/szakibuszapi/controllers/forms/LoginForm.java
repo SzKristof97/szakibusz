13ac50c86176fe1226305e3136461c5551208c6e
Szabó Kristóf Gyula <szkristofgy@gmail.com>
2022-11-25 08:42:33 +0100

    Service, Controls and Error handling is added for registration!

---
+++ szakibuszAPI/src/main/java/me/szkristof/szakibuszapi/controllers/forms/LoginForm.java
@@ -0,0 +1,29 @@
+package me.szkristof.szakibuszapi.controllers.forms;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginForm {
+    String username;
+    String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ szakibuszAPI/src/main/java/me/szkristof/szakibuszapi/repositories/TicketRepository.java
@@ -0,0 +1,6 @@
+package me.szkristof.szakibuszapi.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends Repository{ }
